2422a468fd148c40afc5bd1b2d0a41719b426756
Francisco Javier Ordoñez Araujo <85919310+JavierOrdonezA@users.noreply.github.com>
2024-06-20 13:54:06 +0200

    Delete Double Pendulum directory

--- Double Pendulum/int_rk4.m
+++
@@ -1,9 +0,0 @@
-function [ tout, xout ] = int_rk4(dt,dx,t,x)
-tout = t + dt;
-dt_half = 0.5*dt;
-k1 = dx(t,x);
-k2 = dx(t+dt_half,x+dt_half*k1);
-k3 = dx(t+dt_half,x+dt_half*k2);
-k4 = dx(tout,x+dt*k3);
-xout = x + dt*(k1+2*k2+2*k3+k4)/6;
-end
